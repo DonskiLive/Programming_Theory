bbb018593ab3548c9627bfc039a5e71b5c2f7acb
DonskiLive <vilchynskyy.a@gmail.com>
2021-11-23 11:48:34 +0100

    updated_23.11.21

--- P72-73-React-firebase-project-todo/Practice/todo_project/src/components/config/firebase-config.js
+++ P72-73-React-firebase-project-todo/Practice/todo_project/src/components/config/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBMT5PVhz-kJ8VKZUN6bq3CI6Kp8RnSNpU",
-	authDomain: "personaltodo-b0b8f.firebaseapp.com",
-	projectId: "personaltodo-b0b8f",
-	storageBucket: "personaltodo-b0b8f.appspot.com",
-	messagingSenderId: "562804725023",
-	appId: "1:562804725023:web:f7d0920144c8a96e804c2a"
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
 };
 
 export const fb = initializeApp(firebaseConfig);
