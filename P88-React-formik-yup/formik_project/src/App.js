71734db18d8d2b65a23c6b9b2a0d5ac5f74b0c0a
DonskiLive <vilchynskyy.a@gmail.com>
2021-12-09 13:31:18 +0100

    updated_09.12.21

---
+++ P88-React-formik-yup/formik_project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormikTest from './components/FormikTest';
+
+const App = () => {
+  return (
+    <div className="App">
+      <FormikTest/>
+    </div>
+  );
+}
+
+export default App;
